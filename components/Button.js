f9658a07f12869c480a8459aa8bbe02e0200b825
Layla Habahbeh <laylahabahbeh@lc-laylahabahbeh.local>
2017-08-30 18:16:31 -0700

    Add shared states and mutliple buttons have separate events

--- components/Button.js
+++ components/Button.js
@@ -5,27 +5,15 @@ import React, { Component } from 'react';
   2. need lifecycle methods
   3. need Prototype functions for events */
 class Button extends Component {
-    // needed to add to "stage-2" to the .bablerc file and run npm i -D babel-preset-stage-2
-    state = { 
-        counter: 1 
-    };
-
-    incrementCounter = (event) => {  // if this is incrementCounter() = { .. instead 'this will be null unless you bind it in the' 
-                                  // constructor or in render: onClick={this.incrementCounter.bind(this)} 
-        event.preventDefault(); // won't do default event which is to submit the button here
-        
-        // setState is async so we should use prevState to make sure new state is updated using the last state
-        // WITHOUT () it says begin funcition. WITH parenthesis it says return something, an object
-        this.setState((prevState) =>({ counter: prevState.counter + 1 }));
+    handleClick = (event) => {
+        event.preventDefault();
+        this.props.onClick(this.props.increment);
     }
-
     render(){
-        // component output consists of both props and states
         return (
-            // REACT ELEMENT (not a component) 
-            <button type="submit" onClick={this.incrementCounter}> 
-                {this.state.counter}
-            </button> 
+            <button onClick={this.handleClick}>
+                {this.props.increment > 0 ? '+' : ''}{this.props.increment}
+            </button>
         );
     }
 }
--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import Button from './components/Button';
 
 class App extends Component {
+    // needed to add to "stage-2" to the .bablerc file and run npm i -D babel-preset-stage-2
+    state = {
+      counter: 99
+    };
+
+    incrementCounter = (increment) => {
+      this.setState((prevState) => {
+        return {counter: prevState.counter + increment};
+      });
+    }
+
     render() {
         return (
             <div>
-                <input 
-                    ref={(inputElement) => {
-                        this.inputELement = inputElement;
-                    }} 
-                />
-               <Button label="Save" /> 
+                <Button increment={1} onClick={this.incrementCounter} />
+                <Button increment={10} onClick={this.incrementCounter} />
+                <Button increment={-5} onClick={this.incrementCounter} />
+                <div>{this.state.counter}</div>
             </div>
         );
     }
 }
 
-// render React element. Babel converts to React.createEleemtn("div")
 ReactDOM.render(
     <App />, 
     document.getElementById('root')
